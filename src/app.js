292d291072d47226e699d7504a6bb8f51f96f044
Oscar Lagatta <Oscar.Lagatta@jato.com>
2019-11-19 20:06:31 +0000

    code cleanup

--- src/app.js
+++ src/app.js
@@ -1,40 +1,14 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const Warning = React.lazy(() => import('./warning'));
-
 class App extends React.Component {
-  state = {
-    count: 0
-  };
-
   render() {
     return (
       <div>
         <h1>This is Jato's React boilerplate.</h1>
-        <img src="./logo.png" alt="logo" />
-        <h2 className={this.state.count > 10 ? 'warning' : null}>
-          Count: {this.state.count}
-        </h2>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
-
-        {this.state.count > 10 ? (
-          <React.Suspense fallback={null}>
-            <Warning />
-          </React.Suspense>
-        ) : null}
       </div>
     );
   }
 }
-const hotFunction = hot(module);
-export default hotFunction(App);
+
+export default hot(module)(App);
--- src/warning.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Warning = () => <span className={'warning'}>take it easy</span>;
-
-Warning.displayName = 'Warning';
-
-export default Warning;
