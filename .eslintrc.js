6f4a4ba97317ef497b6e579892fb121534fcf65f
Oscar Lagatta <oscarlagatta@gmail.com>
2019-11-21 22:50:25 +0000

    eslint configuration for emotion css property

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,29 +5,29 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
-    jest: true,
+    jest: true
   },
   settings: {
     react: {
-      version: '16.11.0',
-    },
+      version: '16.11.0'
+    }
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', 'jsx-a11y'],
-  rules: { 'react/no-unescaped-entities': 0 },
+  rules: { 'react/no-unescaped-entities': 0, 'react/no-unknown-property': 0 }
 };
